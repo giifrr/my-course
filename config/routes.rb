9fa95193909527b8cab3690d06238b08c8409c0b
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-08-18 13:25:04 +0700

    Add trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index]
   devise_for :users
   resources :courses
   root to: 'pages#home'
   get 'pages/privacy_policy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
