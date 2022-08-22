4be128a5af3f43ecb2b110b6f2d1723068352735
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-08-22 10:19:14 +0700

    remove confirmable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[ home ]
+  
   def home
     @popular_courses = Course.all.limit(3)
     @most_recent = Course.all.order(created_at: :desc).limit(3)
