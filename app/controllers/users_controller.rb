d566bdc5e82580bb461e3703155494c353adb2aa
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-09-07 01:43:41 +0700

    add pagy to enrollment and user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def index
     @user_ransack = User.ransack(params[:user_search], search_key: :user_search)
     @users = @user_ransack.result(distinct: true)
+    @pagy, @users = pagy(@user_ransack.result(distinct: true))
     authorize @users
   end
 
