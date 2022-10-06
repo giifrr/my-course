582ac682fa0fe18fc7f165f589e447709465024c
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-10-06 14:15:34 +0700

    add dependent restrict_with_error and nullify in course and user

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :course, counter_cache: true
   has_rich_text :content
-  has_many :user_lessons
+  has_many :user_lessons, dependent: :destroy
 
   def view_lesson(user)
     if UserLesson.where(user: user, lesson: self).empty? && self.course.user != user
