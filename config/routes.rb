409a0e6dd5fc06ffe323a1b67653dee558114b81
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-10-08 17:27:12 +0700

    add chartkick and groupdate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   devise_for :users
+
   resources :courses do
     get :mylearning, :mycourse,  on: :collection
     resources :lessons
     resources :enrollments, only: %i[ new create edit update destroy ]
   end
+
   resources :enrollments, only: %i[ index show ]
   get 'mystudents', to: 'enrollments#mystudents'
+
+  namespace :charts do 
+    get 'users_created'
+  end
+
   root to: 'pages#home'
   get 'pages/activity'
   get 'pages/privacy_policy'
